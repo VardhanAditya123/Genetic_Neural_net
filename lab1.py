0d8b8ca0599c3dd66ebce485674f5c09d3e26206
Aditya Vardhan Merla <amerla@cuda.cs.purdue.edu>
2020-09-20 02:33:37 -0400

    Commit

--- lab1.py
+++ lab1.py
@@ -262,8 +262,8 @@ def confMatrix(data, preds):
     for i in range(preds.shape[0]):
         n_preds.append(np.where(preds[i] == 1))
         n_yTest.append(np.where(yTest[i] == 1))
-        print(n_preds.shape)
-        print(n_yTest.shape)
+         print(n_preds)
+        # print(n_yTest.shape)
     print(metrics.confusion_matrix(n_yTest, n_preds, labels=["0","1", "2", "3", "4" , "5" , "6" , "7" , "8" , "9"]))
     
 
