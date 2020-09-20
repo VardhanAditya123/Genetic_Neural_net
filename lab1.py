b054564302bd4a76ab639d992edab9e66ce5bd52
Aditya Vardhan Merla <amerla@cuda.cs.purdue.edu>
2020-09-20 09:46:09 -0400

    Commit

--- lab1.py
+++ lab1.py
@@ -265,7 +265,7 @@ def confMatrix(data, preds):
     for i in range(preds.shape[0]):
         n_preds.append(findMax(preds[i] ))
         n_yTest.append(findMax(yTest[i] ))
-    # labels = ['0','1','2','3','4','5','6','7','8','9']
+    # labels = ['0','1','2','3','4','5','6','7','8','9'] 
     labels = [0,1,2,3,4,5,6,7,8,9]
     confusion = metrics.confusion_matrix(n_yTest, n_preds,labels)
     report = metrics.classification_report(n_yTest, n_preds,labels)
