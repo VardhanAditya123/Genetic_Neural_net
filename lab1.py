e912d1a8705ea4ce8cb78bc48d4a325f79176f29
Aditya Vardhan Merla <amerla@cuda.cs.purdue.edu>
2020-09-20 02:55:58 -0400

    Commit

--- lab1.py
+++ lab1.py
@@ -265,6 +265,7 @@ def confMatrix(data, preds):
     # labels = ['0','1','2','3','4','5','6','7','8','9']
     labels = [0,1,2,3,4,5,6,7,8,9]
     confusion = metrics.confusion_matrix(n_yTest, n_preds,labels)
+    print(confusion.shape)
     print(confusion)
     
 
