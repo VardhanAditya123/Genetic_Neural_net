18dfccbe1cd959b376bd132fe7f7184131273785
Aditya Vardhan Merla <amerla@cuda.cs.purdue.edu>
2020-09-20 12:38:01 -0400

    Commit

--- lab1.py
+++ lab1.py
@@ -180,13 +180,6 @@ def trainANN(model,xTrain,yTrain,epochs=5):
 def runANN(data , model):
     (xTest, yTest) = data 
     preds = model.evaluate(xTest,yTest)
-    print(preds)
-    labels = [0,1,2,3,4,5,6,7,8,9]
-    confusion = metrics.confusion_matrix(yTest, preds,labels)
-    report = metrics.classification_report(yTest,preds,labels)
-    print("\nConfusion Matrix:\n")
-    print(confusion)
-    print("\nReport:")
     print("loss:%f\naccuracy: %f" % tuple(preds))
     
 
