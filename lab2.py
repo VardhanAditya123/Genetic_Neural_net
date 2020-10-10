48bb3d15a64653a89e6a668a23aec5c393ca3f94
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-10 05:29:27 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -73,7 +73,7 @@ def buildTFNeuralNet(x, y, eps = 6):
     tf.keras.layers.Dense(500,activation = tf.nn.sigmoid),
     tf.keras.layers.Dense(10,activation = tf.nn.softmax),
     ])
-    # model.add(keras.layers.Dropout(0.15))
+    model.add(keras.layers.Dropout(0.2))
     model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
     return model
 
