c87dfa3824cbed9127a44adc8d68a2055a78811d
Aditya Vardhan Merla <amerla@cuda.cs.purdue.edu>
2020-10-10 03:39:34 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -69,14 +69,16 @@ def findMax(layer):
 
 def buildTFNeuralNet(x, y, eps = 6):
     model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(100,activation = tf.nn.sigmoid),
-    tf.keras.layers.Dense(70,activation = tf.nn.sigmoid),
-    tf.keras.layers.Dense(30,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(80,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(40,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(10,activation = tf.nn.sigmoid),
     ])
     model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
     return model
 
 def trainANN(model,xTrain,yTrain,epochs=5):
+    model.fit(xTrain,yTrain,epochs=5)
+    model.fit(xTrain,yTrain,epochs=5)
     model.fit(xTrain,yTrain,epochs=5)
     model.fit(xTrain,yTrain,epochs=5)
     return model
