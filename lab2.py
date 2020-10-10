806076fc1dbac3bd2b1ce28d1a0fd284b74dbfe4
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-10 05:13:53 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -71,9 +71,12 @@ def buildTFNeuralNet(x, y, eps = 6):
     model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(80,activation = tf.nn.sigmoid),
     tf.keras.layers.Dense(50,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(50,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(50,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(50,activation = tf.nn.sigmoid),
     tf.keras.layers.Dense(10,activation = tf.nn.softmax),
     ])
-    model.add(keras.layers.Dropout(0.7))
+    model.add(keras.layers.Dropout(0.2))
     model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
     return model
 
