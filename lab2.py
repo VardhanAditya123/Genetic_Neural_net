c618ee0aeb7b72655550d1f3db361f7ef1eb74a6
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-10 10:49:24 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -78,7 +78,7 @@ def buildTFNeuralNet(x, y, eps = 6):
 def trainANN(model,xTrain,yTrain,epochs=5):
     model.fit(xTrain,yTrain,epochs=5)
     model.compile(optimizer='adadelta',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-    model.fit(xTrain,yTrain,epochs=5=10)
+    model.fit(xTrain,yTrain,epochs=10)
     return model
 
 def runANN(data , model):
