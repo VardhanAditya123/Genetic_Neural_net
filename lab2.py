c7207431defc1e6cff80cb4559c5854d6f05e09f
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 05:13:05 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -100,6 +100,9 @@ def trainANN(model,xTrain,yTrain,epochs=5):
         model.fit(xTrain,yTrain,epochs=5)
         model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
         model.fit(xTrain,yTrain,epochs=5)
+        model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+        model.fit(xTrain,yTrain,epochs=5)
+    
 
     return model
 
