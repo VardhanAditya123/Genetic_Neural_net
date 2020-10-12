1bf804253bb7225d4820c1230f2c81ab7d67e987
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 08:56:59 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -202,7 +202,7 @@ def preprocessData(raw):
     print("New shape of xTest dataset: %s." % str(xTestP.shape))
     print("New shape of yTrain dataset: %s." % str(yTrainP.shape))
     print("New shape of yTest dataset: %s." % str(yTestP.shape))
-    return ((xTrainP, yTrain), (xTestP, yTestP))
+    return ((xTrainP, yTrain), (xTestP, yTest))
 
 
 
