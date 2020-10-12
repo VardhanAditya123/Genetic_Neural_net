b1d8d675d9388ec88034e8f96e759de7a7147680
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 09:50:29 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -70,7 +70,7 @@ def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
     model.add(keras.layers.MaxPooling2D(pool_size = (2,2)))
     model.add(tf.keras.layers.Dropout(dropRate))
 
-    model.add(keras.layers.Conv2D(32, kernel_size =(3, 3), activation = "relu", input_shape = inShape))
+    model.add(keras.layers.Conv2D(32, kernel_size =(3, 3), activation = "relu"))
     model.add(tf.keras.layers.Dropout(dropRate))
 
     model.add(keras.layers.Conv2D(64, kernel_size =(3, 3), activation = "relu"))
