5a68c30423176459ed5fdc8b17f6036571ef8f45
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 09:08:56 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -79,6 +79,7 @@ def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
     model.add(keras.layers.Dense(128 , activation = "relu"))
     model.add(keras.layers.Dense(NUM_CLASSES , activation = "softmax"))
     model.compile(optimizer = opt, loss = lossType ,metrics=['accuracy'])
+    
     model.fit(x,y,epochs = 10)
     return model
 
