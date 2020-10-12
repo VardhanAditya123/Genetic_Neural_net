baa8f30fb62584c33ebe0c9917f66a32eca88639
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 05:58:15 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -38,7 +38,11 @@ elif DATASET == "mnist_f":
     IZ = 1
     IS = 784
 elif DATASET == "cifar_10":
-    pass                                 # TODO: Add this case.
+    NUM_CLASSES = 10
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = 3072                                # TODO: Add this case.
 elif DATASET == "cifar_100_f":
     pass                                 # TODO: Add this case.
 elif DATASET == "cifar_100_c":
