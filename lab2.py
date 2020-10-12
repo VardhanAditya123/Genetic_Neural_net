97a7f00e07901f57e10de27f810170b929a4c4aa
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 03:16:56 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -69,8 +69,8 @@ def findMax(layer):
 
 def buildTFNeuralNet(x, y, eps = 6):
     model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512,activation = tf.nn.relu),
-    tf.keras.layers.Dense(150,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(512,activation = tf.nn.sigmoid),
+    tf.keras.layers.Dense(100,activation = tf.nn.sigmoid),
     tf.keras.layers.Dense(100,activation = tf.nn.softmax),
     ])
     model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
