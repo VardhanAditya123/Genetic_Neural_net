00890e53af62b77d561e13cb80c0a01461359f28
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 05:33:43 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -97,7 +97,7 @@ def trainANN(model,xTrain,yTrain,epochs=5):
     
     elif DATASET == "mnist_f":
         model.fit(xTrain,yTrain,epochs=10)
-        model.compile(optimizer='adagrad',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+        model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
         model.fit(xTrain,yTrain,epochs=5)
 
     
