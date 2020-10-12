feedc42ad5395689d58505be1959c74133ffb636
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-12 12:34:27 -0400

    Commit

--- lab2.py
+++ lab2.py
@@ -20,9 +20,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 ALGORITHM = "tf_conv"
 
 # DATASET = "mnist_d"
-# DATASET = "mnist_f"
+DATASET = "mnist_f"
 # DATASET = "cifar_10"
-DATASET = "cifar_100_f" 
+# DATASET = "cifar_100_f" 
 # DATASET = "cifar_100_c"
 
 if DATASET == "mnist_d":
