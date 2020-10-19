0ec03bc9872d381d5247258b289897ffcb50a612
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 12:03:14 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -176,7 +176,7 @@ def compute_loss_and_grads(cData, sData, tData):
         loss = loss + (STYLE_WEIGHT / len(styleLayerNames))* styleLoss(styleOutput,genOutput) 
     
     loss +=  totalLoss(genTensor)
-    grads = K.gradients(loss,genTensor).flatten().astype("float64")
+    grads = (K.gradients(loss,genTensor)).flatten().astype("float64")
 
     return loss,grads
 
