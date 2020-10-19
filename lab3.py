05b95a84793a3502be50c74ec43962b0f1d968af
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 05:59:33 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -173,7 +173,7 @@ def styleTransfer(cData, sData, tData):
 
     evaluator = Evaluator()
 
-     x = np.random.uniform(0, 255, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3)) - 128.
+    x = np.random.uniform(0, 255, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3)) - 128.
 
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
