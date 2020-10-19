57b985f5b4b356a3fadaa8a3991c062acec67fc6
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 12:43:27 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -129,7 +129,7 @@ def styleTransfer(cData, sData, tData ):
     optimizer = tf.train.AdamOptimizer()
 
     for i in range(TRANSFER_ROUNDS):
-        
+        loss, grads = compute_loss_and_grads(combination_image, base_image, style_reference_image)
         print("   Step %d." % i)
         optimizer.apply_gradients([(grads, combination_image)])
         print("   Loss: %f." % loss)
