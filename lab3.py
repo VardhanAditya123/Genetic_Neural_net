c6783d8bd713114a3d42dc76817a19468815fab1
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 23:02:19 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -170,7 +170,7 @@ def styleTransfer(cData, sData, tData):
                 x = x.reshape((1, img_height, img_width, 3))
                 outs = fetch_loss_and_grads([x])
                 loss_value = outs[0]
-                grad_values = outs[1].flatten().astype('float64')
+                grad_values = outs[1]
                 self.loss_value = loss_value
                 self.grad_values = grad_values
                 return self.loss_value
