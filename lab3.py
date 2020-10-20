14a5a5afc0c129e20ed1c6c25063afd9f2b39e40
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 22:55:30 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -178,8 +178,8 @@ def styleTransfer(cData, sData, tData):
         def __init__(self):
                 self.loss_value = None
                 self.grads_values = None
+        
         def loss(self, x):
-                assert self.loss_value is None
                 x = x.reshape((1, img_height, img_width, 3))
                 outs = fetch_loss_and_grads([x])
                 loss_value = outs[0]
@@ -187,8 +187,8 @@ def styleTransfer(cData, sData, tData):
                 self.loss_value = loss_value
                 self.grad_values = grad_values
                 return self.loss_value
+        
         def grads(self, x):
-                assert self.loss_value is not None
                 grad_values = np.copy(self.grad_values)
                 self.loss_value = None
                 self.grad_values = None
