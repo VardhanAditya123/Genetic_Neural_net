05786650938f423190cc8755d29a0c3e7b8d1ebd
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 23:00:53 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -180,7 +180,7 @@ def styleTransfer(cData, sData, tData):
                 self.grads_values = None
         
         def loss(self, x):
-                x = genTensor
+                x = x.reshape((1, img_height, img_width, 3))
                 outs = fetch_loss_and_grads([x])
                 loss_value = outs[0]
                 grad_values = outs[1].flatten().astype('float64')
@@ -190,8 +190,6 @@ def styleTransfer(cData, sData, tData):
         
         def grads(self, x):
                 grad_values = np.copy(self.grad_values)
-                self.loss_value = None
-                self.grad_values = None
                 return grad_values
     
     
