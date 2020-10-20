12139f44c45c2c0bd65dbdde96c95afb09950d4f
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 02:48:19 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -17,8 +17,8 @@ tf.compat.v1.random.set_random_seed(1618)
 tf.compat.v1.logging.set_verbosity( tf.compat.v1.logging.ERROR)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-CONTENT_IMG_PATH = "source2.png"           #TODO: Add this.
-STYLE_IMG_PATH = "style.png"             #TODO: Add this.
+CONTENT_IMG_PATH = "source.png"           #TODO: Add this.
+STYLE_IMG_PATH = "style2.png"             #TODO: Add this.
 OUTPUT_IMG_PATH = "output.png"
 
 CONTENT_IMG_H = 500
