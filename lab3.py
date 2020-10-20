ec2db501f65cae8a2653c719e3e0e90e89b2ad2e
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-19 22:59:22 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -171,7 +171,7 @@ def styleTransfer(cData, sData, tData):
     # kFunction = K.function([genTensor] , outputs)([x])
     loss = totalLoss(c_loss , s_loss)
     # loss = totalLoss2(genTensor)
-    grads = K.gradients(loss, genTensor)
+    grads = K.gradients(loss, genTensor)[0]
     fetch_loss_and_grads = K.function([genTensor], [loss, grads])
 
     class Evaluator(object):
@@ -183,7 +183,7 @@ def styleTransfer(cData, sData, tData):
                 x = x.reshape((1, img_height, img_width, 3))
                 outs = fetch_loss_and_grads([x])
                 loss_value = outs[0]
-                grad_values = outs[1]
+                grad_values = outs[1].flatten().astype('float64')
                 self.loss_value = loss_value
                 self.grad_values = grad_values
                 return self.loss_value
