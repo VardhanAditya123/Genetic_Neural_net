826d79262bc9814c8536e9e69e27ccbb8c5c5481
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 07:18:46 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -182,16 +182,13 @@ def styleTransfer(cData, sData, tData):
     
     
     evaluator = Evaluator()
-    opt = tf.train.AdamOptimizer()
-    grads = opt.compute_gradients(loss, genTensor)
 
     x = np.random.uniform(0, 255, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3)) - 128.
     # x = tData
     x = x.flatten()
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
-        # x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter = 2000)
-        opt.apply_gradients([(evaluator.grads, x)])
+        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter = 30)
         print('Current loss value:', min_val)
         img = x.copy().reshape((img_height, img_width, 3))
         img = deprocess_image(x)
