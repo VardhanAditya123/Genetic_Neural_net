b0c33cca03c62a9a5d19554c9979dfc985a097b9
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 22:02:56 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -184,7 +184,7 @@ def styleTransfer(cData, sData, tData):
         print("   Step %d." % i)
         x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter=1)
        
-        x1 =  x.copy().reshape((img_height, img_width, 3))
+        x1 =  x.copy().reshape((1,img_height, img_width, 3))
         x1 = x1.astype("float64")
         x1 = tf.convert_to_tensor(x1)
         tData = x1
