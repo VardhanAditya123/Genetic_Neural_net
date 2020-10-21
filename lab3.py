989adf9193349ff2c241d65468910bba7caefb2c
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 21:58:04 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -161,7 +161,7 @@ def styleTransfer(cData, sData, tData):
         outputs = [loss]
         outputs += grads
         x = x.reshape((1, IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS))
-        outs = K.function([genTensor], outputs)([x])
+        outs = K.function([tData], outputs)([x])
         loss = outs[0]
         gradients = outs[1].flatten().astype("float64")
         return loss, gradients
@@ -179,12 +179,10 @@ def styleTransfer(cData, sData, tData):
     
     
     evaluator = Evaluator()
-    tData = tf.Variable(tData)
     x = np.random.uniform(0, 255, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3)) - 128
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
         x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter=1)
-        tData = x.astype("float64")
         print('Current loss value:', min_val)
         img = x.copy().reshape((img_height, img_width, 3))
         img = deprocess_image(x)
