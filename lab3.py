960b1d158abc3bef15e29632eea22ca21367a6ee
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 21:19:45 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -184,7 +184,7 @@ def styleTransfer(cData, sData, tData):
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
         x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxfun=30)
-        genTensor=tf.convert_to_tensor(x)
+        genTensor=x
         print('Current loss value:', min_val)
         img = x.copy().reshape((img_height, img_width, 3))
         img = deprocess_image(x)
