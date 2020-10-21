f5681c7a0656aec14ebc2191c16172496f87618e
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 22:14:09 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -180,6 +180,10 @@ def styleTransfer(cData, sData, tData):
     
     evaluator = Evaluator()
     x = np.random.uniform(0, 255, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3)) - 128
+    x1 =  x.copy().reshape((1,img_height, img_width, 3))
+    x1 = x1.astype("float64")
+    x1 = tf.convert_to_tensor(x1)
+    tData = x1
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
         x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter=1)
