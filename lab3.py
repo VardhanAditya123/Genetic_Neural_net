e2df7a9eef5b715181d7e1e949d025f106040719
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 23:11:17 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -170,16 +170,12 @@ def styleTransfer(cData, sData, tData):
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
         loss,grads = compute_loss(cData, sData, tData)
-        # outputs = [loss]
-        # outputs += grads
-       
-        # x = x.reshape((1, IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS))
-        # outs = K.function([tData], outputs)([x])
-        # loss = outs[0]
-        # grads = outs[1]
-        print(type(grads))
-        grads = tf.make_ndarray(grads)
-        # grads = grads.numpy()
+        
+        outputs = grads
+        outs = K.function([tData], outputs)
+        grads = outs
+
+    
         gradients = grads.reshape(1,img_height, img_width, 3)
         gradients=  gradients.astype("float64")
        
