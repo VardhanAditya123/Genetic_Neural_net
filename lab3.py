5b0de8182ece0c040d6a14ce36d1d005d83d25f7
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-20 23:08:04 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -178,8 +178,8 @@ def styleTransfer(cData, sData, tData):
         # loss = outs[0]
         # grads = outs[1]
         print(type(grads))
-        print(grads)
-        grads = grads.numpy()
+        grads = tf.make_ndarray(grads)
+        # grads = grads.numpy()
         gradients = grads.reshape(1,img_height, img_width, 3)
         gradients=  gradients.astype("float64")
        
