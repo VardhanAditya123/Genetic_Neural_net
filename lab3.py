14943bdd64e78754b77a1f93d56f837c42472c12
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-21 06:11:46 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -174,7 +174,7 @@ def styleTransfer(cData, sData, tData):
     x = x.flatten()
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
-        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter= 50)
+        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter= 400)
         print('Current loss value:', min_val)
         img = x.copy().reshape((img_height, img_width, 3))
         img = deprocess_image(x)
