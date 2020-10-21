2c6072cd272e53b63feda504ac60ff627ff44ad1
VardhanAdity123 <VardhanAditya123@github.com>
2020-10-21 06:05:19 -0400

    Commit

--- lab3.py
+++ lab3.py
@@ -170,17 +170,17 @@ def styleTransfer(cData, sData, tData):
     evaluator = Evaluator()
 
     x = np.random.uniform(0, 255, (1, IMAGE_HEIGHT, IMAGE_WIDTH, 3)) - 128.
-    x = tData
+    # x = tData
     x = x.flatten()
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
-        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxfun = 50)
+        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x, fprime=evaluator.grads, maxiter= 1)
         print('Current loss value:', min_val)
         img = x.copy().reshape((img_height, img_width, 3))
         img = deprocess_image(x)
         img = array_to_img(img)
         saveFile = img.save( OUTPUT_IMG_PATH )   #TODO: Implement.
-        imsave(saveFile, img)   #Uncomment when everything is working right.
+        # imsave(saveFile, img)   #Uncomment when everything is working right.
         print("      Image saved to \"%s\"." % saveFile)
         print("   Transfer complete.")
        
