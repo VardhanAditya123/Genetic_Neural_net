baef38fb0b4dbf7b012e59a0c26dee6e495a6a3e
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-23 12:54:17 -0500

    Commit

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -153,8 +153,9 @@ def buildGAN(images, epochs = 40000, batchSize = 32, loggingInterval = 0):
 
         # Train discriminator with a true and false batch
         arr = np.random.randint(0, images.shape[0], batchSize)
-        batch = np.random.randint()
-        for i in range(arr.shape[0]):
+        # batch = images[np.random.randint(0, images.shape[0], batchSize)]
+        global batch
+        for i in range(batchSize):
             batch[i]=images[arr[i]]
         noise = np.random.normal(0, 1, (batchSize, NOISE_SIZE))
         genImages = generator.predict(noise)
