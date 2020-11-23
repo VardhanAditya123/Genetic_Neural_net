ec5415d4ea932437a6908e1623fee113e77687d5
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-23 07:13:06 -0500

    Commit

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -116,6 +116,21 @@ def buildGenerator():
     #       mnist_f (28 x 28 x 1) image
 
     # Creating a Keras Model out of the network
+    model.add(Dense(256 , input_dim = NOISE_SIZE))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(512))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(1024))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(IMAGE_SIZE , actiavtion = "tanh"))
+    model.add(Reshape(IMAGE_SHAPE))
+    inputTensor = Input(shape=(NOISE_SIZE,))
+    return Model(inputTensor,model(inputTensor))
+
+
     inputTensor = Input(shape = (NOISE_SIZE,))
     return Model(inputTensor, model(inputTensor))
 
