6419967427505bf18634361251c0a0f8b425dfd4
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-26 13:06:47 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -234,10 +234,10 @@ def confMatrix(data, preds):
     labels = [0,1,2,3,4,5,6,7,8,9]
     confusion = metrics.confusion_matrix(n_yTest, n_preds,labels)
     report = metrics.classification_report(n_yTest, n_preds,labels)
-    # print("\nConfusion Matrix:\n")
-    # print(confusion)
-    # print("\nReport:")
-    # print(report)   
+    print("\nConfusion Matrix:\n")
+    print(confusion)
+    print("\nReport:")
+    print(report)   
 
 #=========================<Pipeline Functions>==================================
 
