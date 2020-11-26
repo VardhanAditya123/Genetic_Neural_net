38e6d00a243ef0a6ae310e55909bbc59e40e032c
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-26 14:02:05 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -346,17 +346,17 @@ def main():
     raw = getRawData()
     data = preprocessData(raw)
 
-    individuals = []
-    for i in range(no_of_individuals):
-        model = buildModel()
-        individuals.append(model)
+    # individuals = []
+    # for i in range(no_of_individuals):
+    #     model = buildModel()
+    #     individuals.append(model)
     
     # for generation in range(no_of_generations):
     #     print("================<NEXT GENERATION>===================")
     #     individuals = train_nets(data, individuals)
     #     individuals = evolve(individuals)
     
-    model =  individuals[0]
+    model =  buildModel()
     preds = runModel(data[1][0], model)
     evalResults(data[1], preds)
 
