0b3c0a144731dbce16cb64ca5b5add395baa453c
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-26 08:19:44 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -146,7 +146,7 @@ def mutate(new_individual):
     genes = []
     CUSTOM = 1
     mute_individual = NeuralNetwork_NLayer(CUSTOM,IMAGE_SIZE,NUM_CLASSES,NEURONS_PER_LAYER,NO_OF_LAYERS,NETCOUNT,0.1)
-    for x in range (len(NO_OF_LAYERS)):
+    for x in range (NO_OF_LAYERS):
         gene = new_individual[x]
         rows = gene.shape[0]
         col1 = gene.shape[1]
