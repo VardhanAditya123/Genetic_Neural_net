f58d325bb3034283104ed0f40ca91e2f18c09856
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-27 03:44:32 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -195,7 +195,7 @@ def crossover(individuals):
 def train_nets(data,individuals):
     i = 0
     (xTrain , yTrain) = data[0]
-    while i < 60000:
+    while i < 20:
         for individual in individuals:
             x = xTrain[i]
             y = yTrain[i]
