0ba00e291f509bda631a1a7d6df1bb5ef2872cb8
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-28 12:05:24 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -185,8 +185,8 @@ def mutate(new_individual):
     arr = new_individual.W[x]
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
-            n = np.random.rand()
-            if(n < 0.5):
+            n = np.random.randint(150)
+            if(n < 9):
                 arr[i][j] = np.random.rand()
     
     new_individual.W[x] = arr.copy()
