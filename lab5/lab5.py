384bdef4d407666d1d2afb5c3afefdaef01d2ac1
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-28 06:30:38 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -240,11 +240,11 @@ def evolve(individuals):
     ln = len(parents)
     for i1 in range(ln , ln + 3):
         if individuals[i1].random_select > np.random.rand():
-            parents.append(individuals[i1])
+            parents.append(mutate(individuals[i1]))
 
-    for individual in parents:
-        if individual.mutate_chance > np.random.rand():
-            individual = mutate(individual)
+    # for individual in parents:
+    #     if individual.mutate_chance > np.random.rand():
+    #         individual = mutate(individual)
 
     parents_length = len(parents)
     desired_length = no_of_individuals - parents_length
