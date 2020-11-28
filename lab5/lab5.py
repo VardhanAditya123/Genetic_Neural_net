afe61f180f9ade547591c2e949ff24f0384f7845
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-28 12:53:29 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -28,7 +28,7 @@ NUM_CLASSES = 10
 IMAGE_SIZE = 784
 # For N layer custom net
 NO_OF_LAYERS = 3
-NEURONS_PER_LAYER = 6
+NEURONS_PER_LAYER = 30
 no_of_generations = 70
 no_of_individuals = 15
 mutate_factor = 0.15
