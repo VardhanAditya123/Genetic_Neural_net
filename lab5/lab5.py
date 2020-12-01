1f5c73b0034612873c98d6e7772d3157a9673ec1
VardhanAdity123 <VardhanAditya123@github.com>
2020-11-30 23:32:58 -0500

    Commit

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -27,7 +27,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 NUM_CLASSES = 10
 IMAGE_SIZE = 784
 # For N layer custom net
-NO_OF_LAYERS = 6
+NO_OF_LAYERS = 2
 NEURONS_PER_LAYER = 100
 no_of_generations = 40
 no_of_individuals = 6
